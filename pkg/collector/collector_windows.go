140ab6d5ab055e54a00b581b9c83dc4eac2621ce
Tom Charles <tcharles@pivotal.io>
2018-12-17 12:20:09 -0700

    wip: separate load logic for windows and linux
    Signed-off-by: John Cornish <jcornish@pivotal.io>

---
+++ pkg/collector/collector_windows.go
@@ -0,0 +1,13 @@
+// +build windows
+
+package collector
+
+import (
+	"context"
+
+	"github.com/shirou/gopsutil/load"
+)
+
+func (s defaultRawCollector) AvgWithContext(ctx context.Context) (*load.AvgStat, error) {
+	return &load.AvgStat{}, nil
+}
