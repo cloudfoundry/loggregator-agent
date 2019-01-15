ee5c8e08466eb7a7525725503e3ed71006397cff
Travis Patterson <tpatterson@pivotal.io>
2019-01-15 14:55:22 -0700

    Add metric-scraper binary
    [#163206790]
    
    Signed-off-by: Travis Patterson <tpatterson@pivotal.io>

---
+++ cmd/metric-scraper/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	loggregator "code.cloudfoundry.org/go-loggregator"
+	"code.cloudfoundry.org/loggregator-agent/pkg/scraper"
+)
+
+func main() {
+	log := log.New(os.Stderr, "", log.LstdFlags)
+	log.Printf("starting Prometheus Scraper...")
+	defer log.Printf("closing Prometheus Scraper...")
+
+	cfg := loadConfig(log)
+
+	creds, err := loggregator.NewIngressTLSConfig(
+		cfg.CACertPath,
+		cfg.ClientCertPath,
+		cfg.ClientKeyPath,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := loggregator.NewIngressClient(
+		creds,
+		loggregator.WithAddr(cfg.LoggregatorIngressAddr),
+		loggregator.WithLogger(log),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var scrapers []*scraper.Scraper
+	scrapers = append(scrapers, scraper.New(
+		cfg.SourceID,
+		"http://127.0.0.1:"+cfg.ScrapePort+"/metrics",
+		client,
+		http.DefaultClient,
+	))
+
+	for range time.Tick(cfg.ScrapeInterval) {
+		for _, scraper := range scrapers {
+			if err := scraper.Scrape(); err != nil {
+				log.Printf("failed to scrape: %s", err)
+			}
+		}
+	}
+}
